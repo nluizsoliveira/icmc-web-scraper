63c9bed49a1cc93b595bbc9e9078e043d48ff024
Nelson Luiz Serpa de Oliveira <USUARIOS+9793502@6-306-20.grad.usricmc.icmc.usp.br>
2019-12-06 15:31:35 -0300

    Working for a single page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const querystring = require('querystring')
+let jsdom = require('jsdom').JSDOM
 
 async function pega_carai(){
 
@@ -12,7 +13,28 @@ async function pega_carai(){
         },
     })
 
-    console.log(res.data)
+ 
+    // some hard coded html
+    html = res.data
+     
+    // get the dom by calling the jsdom constructor, and giving it the html
+    dom = new jsdom(html),
+     
+    // get the window object @ dom.window
+    window = dom.window,
+     
+    // now just do whatever, just like in the browser
+    elements = window.document.getElementsByClassName("col-xs-6 col-sm-4 col-md-3 col-lg-2")
+
+
+    for(let i = 0; i < elements.length; i++){
+        let nusp = elements[i].children[0].children[0].href.replace('/pessoas?id=','');
+
+        console.log(elements[i].children[0].children[1].children[0].children[0].innerHTML + " | " +  (nusp - 3)/2)
+    }
+
+    
 }
 
 pega_carai()
+
